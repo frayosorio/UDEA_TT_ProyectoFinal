2015de960971af3e660a40c50a4fcd42da4d06fa
fray osorio <frayosorio@gmail.com>
2025-08-05 16:25:49 -0500

    Se agrega visualizacion mediante grafica de kw/m2

--- codigo/controladorRadiacionSolar.js
+++ codigo/controladorRadiacionSolar.js
@@ -6,7 +6,7 @@ app.controller('ControladorRadiacionSolar', function ($scope, $http, ServicioCon
     $scope.desde = new Date();
     $scope.hasta = new Date();
     $scope.desde.setDate($scope.hasta.getDate() - 30);
-
+    $scope.ciudadSeleccionada = null;
 
     $http.get('./datos/CoordenadasColombia.json')
         .then(function (respuesta) {
@@ -23,23 +23,46 @@ app.controller('ControladorRadiacionSolar', function ($scope, $http, ServicioCon
         return `${year}${month}${day}`;
     }
 
-    $scope.consultarRadiacion = function (ciudad) {
+    //activar actualizacion cuando cambie fechas
+    $scope.$watchGroup(['desde', 'hasta'], function (newValues, oldValues) {
+        if ($scope.ciudadSeleccionada && $scope.desde && $scope.hasta) {
+            $scope.consultarRadiacion($scope.ciudadSeleccionada);
+        }
+    });
 
+    $scope.consultarRadiacion = function (ciudad) {
+        $scope.ciudadSeleccionada = ciudad;
         const inicio = formatearFecha($scope.desde);
         const fin = formatearFecha($scope.hasta);
         ServicioConsultas.consultarRadiacion(ciudad.lat,
             ciudad.lon, inicio, fin
         ).then(function (resultado) {
-            $scope.radiacion = resultado.data.properties.parameter.ALLSKY_SFC_SW_DWN;
-
-            $scope.actualizarGrafica();
+            datos = resultado.data.properties.parameter.ALLSKY_SFC_SW_DWN;
+            $scope.radiacion = datos;
+            const x = Object.keys(datos);
+            const y = Object.values(datos);
+            $scope.actualizarGrafica(x, y);
         }
         );
 
     }
 
-    $scope.actualizarGrafica = function () {
+    $scope.actualizarGrafica = function (x, y) {
         const canvas = document.getElementById("grafico");
-        
+        const ctx = canvas.getContext("2d");
+
+        if ($scope.grafico) $scope.grafico.destroy();
+
+        $scope.grafico = new Chart(ctx, {
+            type: "line",
+            data: {
+                labels: x,
+                datasets: [{
+                    label: "Radiación solar(kWh/m²)",
+                    data: y
+                }
+                ]
+            }
+        });
     }
 });
