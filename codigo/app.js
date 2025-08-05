5028c5370635cd0c6cbed10d50ae2dcab7980177
fray osorio <frayosorio@gmail.com>
2025-08-05 10:57:12 -0500

    Se crea menu principal

--- codigo/app.js
+++ codigo/app.js
@@ -1,50 +1,19 @@
-angular.module("appProyecto", [])
-    .controller("controladorProyecto", function ($scope, $http) {
+const app=angular.module("appProyecto", ['ngRoute']);
 
-        $scope.ciudades = [];
-        $scope.ciudadSeleccionada = null;
-        $scope.desde = new Date();
-        $scope.hasta = new Date();
-        $scope.desde.setDate($scope.hasta.getDate() - 30);
-        $scope.radiacion = null;
-
-        $http.get("./datos/CoordenadasColombia.json")
-            .then(function (respuesta) {
-                $scope.ciudades = respuesta.data;
-            })
-
-        function formatearFecha(fecha) {
-            const año = fecha.getFullYear();
-            const mes = (fecha.getMonth() + 1).toString().padStart(2, "0");
-            const dia = (fecha.getDate()).toString().padStart(2, "0");
-            return `${año}${mes}${dia}`;
-        }
-
-        $scope.consultarRadiacion = function (ciudadSeleccionada) {
-            if (!ciudadSeleccionada) {
-                return;
-            }
-            const url = "https://power.larc.nasa.gov/api/temporal/daily/point";
-            const parametros = {
-                parameters: "ALLSKY_SFC_SW_DWN",
-                start: formatearFecha($scope.desde),
-                end: formatearFecha($scope.hasta),
-                latitude: ciudadSeleccionada.lat,
-                longitude: ciudadSeleccionada.lon,
-                format: "JSON",
-                community: "RE"
-            }
-
-            const consulta = Object.entries(parametros)
-                .map(([clave, valor]) => `${clave}=${valor}`)
-                .join("&");
-
-            const urlDefinitiva = `${url}?${consulta}`;
-
-            $http.get(urlDefinitiva)
-                .then(function (respuesta) {
-                    const datos = respuesta.data.properties.parameter.ALLSKY_SFC_SW_DWN;
-                    $scope.radiacion = datos;
-                });
-        }
+app.config(function($routeProvider) {
+  $routeProvider
+    .when('/inicio', {
+      templateUrl: 'vistas/Inicio.html'
+    })
+    .when('/radiacion', {
+      templateUrl: 'vistas/RadiacionSolar.html',
+      controller: 'ControladorRadiacionSolar'
+    })
+    .when('/consumo', {
+      templateUrl: 'vistas/Consumo.html',
+      controller: 'ControladorConsumo'
+    })
+    .otherwise({
+      redirectTo: '/inicio' // Página por defecto
     });
+});
